0d261d2607ce45bb00f41e778c9fb417e769bc5b
jinfang.wei <jinfang.wei@d-robotics.cc>
2025-01-15 11:40:46 +0800

    fix en doc about the logo click link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 // src/pages/index.js
 import React, { useEffect } from 'react';
 import { useHistory } from '@docusaurus/router';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 function HomeRedirect() {
   const history = useHistory();
@@ -9,10 +10,17 @@ function HomeRedirect() {
   //   // 重定向到外部 URL
   //   window.location.href = 'https://d-robotics.cc/';
   // }, []);
-
+  const { i18n } = useDocusaurusContext();
+  console.log(i18n.currentLocale)
   useEffect(() => {
     // 重定向到文档的首页路径
-    history.push('/rdk_doc/RDK');
+    if(i18n.currentLocale==="zh-Hans"){
+      history.push('/rdk_doc/RDK');
+    }else{
+      history.push('/rdk_doc/en/RDK');
+    }
+      
+
   }, [history]);
 
   return null; // 不渲染任何内容
