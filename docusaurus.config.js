669d7fac4c92cbecc96eb65fb09fdc0c47ccb5c3
GuanzhongChen <2564772558@qq.com>
2025-06-19 17:07:40 +0800

    optimize display

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -90,6 +90,12 @@ const config = {
     ({
       // Replace with your project's social card
       image: "img/docusaurus-social-card.jpg",
+      
+                // ✅ 新增：支持 h2 ~ h5 add by xgs for table of contents
+    tableOfContents: {
+      minHeadingLevel: 2,
+      maxHeadingLevel: 5,
+    },
       navbar: {
         title: "D-Robotics",
         logo: {
